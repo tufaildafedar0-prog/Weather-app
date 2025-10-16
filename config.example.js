62cd14c13c35d6919d0c53d142370c4d5b84ec97
Tufailhasan <tufaildafedar0@gmail.com>
2025-10-16 23:45:52 +0530

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -3,5 +3,6 @@
 // Copy this file to 'config.js' and replace the placeholder with your OpenWeatherMap API key.
 // Example: window.APP_CONFIG = { apiKey: 'your_real_key_here' };
 window.APP_CONFIG = {
-apiKey: 'YOUR_OPENWEATHERMAP_API_KEY_HERE'
+apiKey: 'dff29f41b8f3646f7af65f935118f0d5'
 };
+
